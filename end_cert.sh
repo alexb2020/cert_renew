f9a9356391ab5deca7a1a6a74f4fd95157e0a2bb
Alexander Faria <alexanderjfb@gmail.com>
2017-11-19 22:25:17 -0300

    Se agrega cndicion para renovar solo si tienen menos de 30 dias de vigencia SOlo reiniica el servidor web si renueva el certificado Envia diferentes mensajes en el correo

--- end_cert.sh
+++ end_cert.sh
@@ -5,25 +5,30 @@
 
 #En este bloque se optiene la fecha en la cual expirara el certificado
 
-        FECHA_STRING=$(echo | openssl s_client -connect localhost:443 2>/dev/null | openssl x509 -noout -dates | grep "notAfter")
+        FECHA_STRING=$(echo | openssl s_client -connect miprofe.com:443 2>/dev/null | openssl x509 -noout -dates | grep "notAfter")
         FECHA_FIN_SEG=$(date -d "${FECHA_STRING:9:20}" +%s)
         FECHA_ACTUAL=$(date +%s)
         DIAS=$(((FECHA_FIN_SEG - FECHA_ACTUAL)/(60*60*24)))
-
+        
 
 #Crea el archivo de log
+    if [ $DIAS -ge 30 ]; then
         dt=`date +%y%m%d`
         host=$(cat /etc/hostname)
         echo -e "Subject: El certificado de $host tiene $DIAS dias de vigencia \n" > cert-renew-$dt
-
+        echo -e "El certificado sera renovado cuando su vigencia sea menor a 30 dias" >> cert-renew-$dt
+    else
+        dt=`date +%y%m%d`
+        host=$(cat /etc/hostname)
+        echo -e "Subject: El certificado de $host fue renovado \n" > cert-renew-$dt
 #Ejecuta  el bot  de renovacion. Actualiza cerbot y renueva los certificados que esten por vencer.
         sudo letsencrypt renew >> cert-renew-$dt
-
+        renew=1
+    fi
 #si se quiere enviar el correo solo si se realizo la renovacion se descomente las lineas 23,24 y 29.
-#        verf=$(grep -c "No" cert-renew-$dt)
-#        if [ $verf = 0 ];then
-             sendmail correo < cert-renew-$dt
-#            sendmail correo < cert-renew-$dt # se pueden enviar a varas cuentas
-        #Reinicia el servidor apache
-             service apache2 restart
-#        fi
+    sendmail correo < cert-renew-$dt
+#   sendmail correo < cert-renew-$dt # se pueden enviar a varas cuentas
+        #Reinicia el servidor apacher
+    if [ $renew ]
+        service apache2 restart
+    fi
